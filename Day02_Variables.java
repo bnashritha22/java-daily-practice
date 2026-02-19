cd686f1eb140d04684169d7089d4139b76c0c1a0
BN Ashritha <bnashritha22@gmail.com>
2026-02-19 17:57:18 +0530

    Update Day02_Variables.java

--- Day02_Variables.java
+++ Day02_Variables.java
@@ -16,6 +16,6 @@ public class Day02_Variables {
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
         System.out.println("Height: " + height);
-        System.out.println("Student? " + isStudent);
+        System.out.println("Student? " + isStudent); 
     }
 }
