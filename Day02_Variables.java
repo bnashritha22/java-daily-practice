cc2d2cb7081c72e0e2c101c19aeee2e924fe5f4a
BN Ashritha <bnashritha22@gmail.com>
2026-02-19 17:51:46 +0530

    Create Day02_Variables.java
    Day 2  Variables practice

---
+++ Day02_Variables.java
@@ -0,0 +1,21 @@
+public class Day02_Variables {
+    public static void main(String[] args) {
+        // Integer variable
+        int age = 20;
+
+        // Decimal number
+        double height = 5.7;
+
+        // Text
+        String name = "Ashritha";
+
+        // Boolean
+        boolean isStudent = true;
+
+        // Print all variables
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Student? " + isStudent);
+    }
+}
