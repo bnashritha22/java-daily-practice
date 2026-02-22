db722d6e3089441e466fddceafc426a5660b30e3
BN Ashritha <bnashritha22@gmail.com>
2026-02-22 11:49:35 +0530

    Update Day05_ForLoop.java
    Day 5 -Added reverse loop

--- Day05_ForLoop.java
+++ Day05_ForLoop.java
@@ -2,6 +2,11 @@ public class Day05_ForLoop {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
             System.out.println("Number: " + i);
+            System.out.println("Reverse counting:");
+
+for (int i = 5; i >= 1; i--) {
+    System.out.println(i);
+}
         }
     }
 }
