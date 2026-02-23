bbfd6d0d8fd8d7a95257d048c90edd4031411014
BN Ashritha <bnashritha22@gmail.com>
2026-02-23 20:26:14 +0530

    Create Day06_EvenNumbers.java
    Day 6 - Printed even numbers using while loop

---
+++ Day06_EvenNumbers.java
@@ -0,0 +1,10 @@
+public class Day06_EvenNumbers {
+    public static void main(String[] args) {
+        int num = 2;
+
+        while (num <= 10) {
+            System.out.println(num);
+            num += 2;
+        }
+    }
+}
