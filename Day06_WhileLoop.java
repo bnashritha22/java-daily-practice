f9471544a4a3562a992f0dc8962e78d5a19430f3
BN Ashritha <bnashritha22@gmail.com>
2026-02-23 20:25:16 +0530

    Update Day06_WhileLoop.java
    Day 6 - Added reverse while loop

--- Day06_WhileLoop.java
+++ Day06_WhileLoop.java
@@ -5,6 +5,14 @@ public class Day06_WhileLoop {
         while (i <= 5) {
             System.out.println("Count: " + i);
             i++;
+
+            System.out.println("Reverse counting:");
+
+int j = 5;
+while (j >= 1) {
+    System.out.println(j);
+    j--;
+}
         }
     }
 }
