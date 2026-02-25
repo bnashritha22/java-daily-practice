ac552d1a1ea11087ddb32b64c5ebe63defca5f77
BN Ashritha <bnashritha22@gmail.com>
2026-02-25 18:35:51 +0530

    Create Day08_Methods.java
    Day 8- Created first method

---
+++ Day08_Methods.java
@@ -0,0 +1,11 @@
+public class Day08_Methods {
+
+    // method that prints a message
+    public static void greet() {
+        System.out.println("Hello, welcome to Java practice!");
+    }
+
+    public static void main(String[] args) {
+        greet();  // calling the method
+    }
+}
