e2e1607ef6ad1168f7932643baef54a7f6fea4e4
BN Ashritha <bnashritha22@gmail.com>
2026-02-25 18:40:30 +0530

    Update Day08_Methods.java
    Day 8 - Added method with parameter

--- Day08_Methods.java
+++ Day08_Methods.java
@@ -1,6 +1,10 @@
 public class Day08_Methods {
 
     // method that prints a message
+    public static void greetUser(String name) {
+    System.out.println("Hello " + name + "!");
+        greetUser("Ashritha");
+}
     public static void greet() {
         System.out.println("Hello, welcome to Java practice!");
     }
