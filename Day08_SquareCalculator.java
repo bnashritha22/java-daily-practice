e319e9e3455ce0af6c986a0496faaf3fcd6dfb63
BN Ashritha <bnashritha22@gmail.com>
2026-02-25 18:42:23 +0530

    Create Day08_SquareCalculator.java
    Day 8 - Built square calculator method

---
+++ Day08_SquareCalculator.java
@@ -0,0 +1,11 @@
+public class Day08_SquareCalculator {
+
+    public static int square(int num) {
+        return num * num;
+    }
+
+    public static void main(String[] args) {
+        int result = square(4);
+        System.out.println("Square: " + result);
+    }
+}
