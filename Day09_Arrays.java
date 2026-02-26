015255d1660e0c53e1b3d2339fc91634622737b9
BN Ashritha <bnashritha22@gmail.com>
2026-02-26 21:38:25 +0530

    Create Day09_Arrays.java
    Day 9 - Created first array

---
+++ Day09_Arrays.java
@@ -0,0 +1,9 @@
+public class Day09_Arrays {
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("First element: " + numbers[0]);
+        System.out.println("Second element: " + numbers[1]);
+    }
+}
