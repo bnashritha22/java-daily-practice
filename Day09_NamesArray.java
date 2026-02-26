bbd405508eba33c7f169212b6bc2c0a7d26f7888
BN Ashritha <bnashritha22@gmail.com>
2026-02-26 21:42:24 +0530

    Create Day09_NamesArray.java
    Day 9 - Practiced string array

---
+++ Day09_NamesArray.java
@@ -0,0 +1,10 @@
+public class Day09_NamesArray {
+    public static void main(String[] args) {
+
+        String[] names = {"Ashritha", "Ravi", "Priya"};
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+    }
+}
