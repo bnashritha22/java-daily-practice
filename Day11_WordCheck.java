a66723439274ef2ed86e89c0aa6069700a2932aa
BN Ashritha <bnashritha22@gmail.com>
2026-02-28 12:31:24 +0530

    Create Day11_WordCheck.java
    Day 11 — Word length checker

---
+++ Day11_WordCheck.java
@@ -0,0 +1,12 @@
+public class Day11_WordCheck {
+    public static void main(String[] args) {
+
+        String word = "Java";
+
+        if (word.length() > 3) {
+            System.out.println("Word is long");
+        } else {
+            System.out.println("Word is short");
+        }
+    }
+}
