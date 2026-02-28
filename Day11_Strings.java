b487ef291d76ce7c82d7178f13d89509066c70f8
BN Ashritha <bnashritha22@gmail.com>
2026-02-28 12:28:53 +0530

    Create Day11_Strings.java
    Day 11 — Basic string operations

---
+++ Day11_Strings.java
@@ -0,0 +1,11 @@
+public class Day11_Strings {
+    public static void main(String[] args) {
+
+        String message = "Java Learning";
+
+        System.out.println("Text: " + message);
+        System.out.println("Length: " + message.length());
+        System.out.println("Uppercase: " + message.toUpperCase());
+        System.out.println("Lowercase: " + message.toLowerCase());
+    }
+}
