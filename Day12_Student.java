a6207be0b19dddda666db5902d239a5634f4f43f
BN Ashritha <bnashritha22@gmail.com>
2026-03-01 09:34:39 +0530

    Update Day12_Student.java
    Day 12 — Created multiple objects

--- Day12_Student.java
+++ Day12_Student.java
@@ -9,11 +9,16 @@ public class Day12_Student {
     }
 
     public static void main(String[] args) {
-        Day12_Student student1 = new Day12_Student();
+        
+          Day12_Student student1 = new Day12_Student();
 
         student1.name = "Ashritha";
         student1.age = 20;
+Day12_Student student2 = new Day12_Student();
+student2.name = "Ravi";
+student2.age = 22;
 
+student2.displayInfo();
         student1.displayInfo();
     }
 }
