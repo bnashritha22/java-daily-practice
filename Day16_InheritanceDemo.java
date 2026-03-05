db7c26fea215d6d11572334b96f3a656f5877a8e
BN Ashritha <bnashritha22@gmail.com>
2026-03-05 12:55:11 +0530

    Create Day16_InheritanceDemo.java
    Day 16 — Implemented basic inheritance

---
+++ Day16_InheritanceDemo.java
@@ -0,0 +1,24 @@
+class Animal {
+
+    void eat() {
+        System.out.println("This animal eats food");
+    }
+}
+
+class Dog extends Animal {
+
+    void bark() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class Day16_InheritanceDemo {
+
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+
+        d.eat();   // inherited method
+        d.bark();  // own method
+    }
+}
