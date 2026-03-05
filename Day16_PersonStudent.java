4df96b6fc11075393d54d7495290f603acae3131
BN Ashritha <bnashritha22@gmail.com>
2026-03-05 12:55:57 +0530

    Create Day16_PersonStudent.java
    Day 16 — Practiced inheritance with Person and Student classes

---
+++ Day16_PersonStudent.java
@@ -0,0 +1,31 @@
+class Person {
+
+    String name;
+
+    void displayName() {
+        System.out.println("Name: " + name);
+    }
+}
+
+class Student extends Person {
+
+    int marks;
+
+    void displayMarks() {
+        System.out.println("Marks: " + marks);
+    }
+}
+
+public class Day16_PersonStudent {
+
+    public static void main(String[] args) {
+
+        Student s = new Student();
+
+        s.name = "Ashritha";
+        s.marks = 95;
+
+        s.displayName();
+        s.displayMarks();
+    }
+}
