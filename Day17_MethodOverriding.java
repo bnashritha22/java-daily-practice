3858afd7886761c96f017b44f8f0fb2f7e0c2ddc
BN Ashritha <bnashritha22@gmail.com>
2026-03-06 23:07:16 +0530

    Create Day17_MethodOverriding.java
    Day 17 — Implemented method overriding

---
+++ Day17_MethodOverriding.java
@@ -0,0 +1,23 @@
+class Animal {
+
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class Day17_MethodOverriding {
+
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+        d.sound();
+    }
+}
