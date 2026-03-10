3a3b11f18cde04ebb56f58a572e310ec826e6f35
BN Ashritha <bnashritha22@gmail.com>
2026-03-10 20:32:58 +0530

    Create Day21_ArrayListExample.java
    Day 21 — Created basic ArrayList example

---
+++ Day21_ArrayListExample.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Day21_ArrayListExample {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> fruits = new ArrayList<>();
+
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Mango");
+
+        System.out.println(fruits);
+    }
+}
