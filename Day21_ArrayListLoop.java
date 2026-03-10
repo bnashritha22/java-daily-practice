31713597032e0529c80075e193614e1b51b26bd2
BN Ashritha <bnashritha22@gmail.com>
2026-03-10 20:34:42 +0530

    Create Day21_ArrayListLoop.java
    Day 21 — Iterated through ArrayList using loop

---
+++ Day21_ArrayListLoop.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Day21_ArrayListLoop {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Anshu");
+        names.add("Riya");
+        names.add("Rahul");
+
+        for(String name : names) {
+            System.out.println(name);
+        }
+    }
+}
