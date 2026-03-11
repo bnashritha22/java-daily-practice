324efccbbba44368377edaefbd7dabc568149471
BN Ashritha <bnashritha22@gmail.com>
2026-03-11 21:32:08 +0530

    Create Day22_HashMapExample.java
    Day 22 — Created basic HashMap example

---
+++ Day22_HashMapExample.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class Day22_HashMapExample {
+
+    public static void main(String[] args) {
+
+        HashMap<String, Integer> students = new HashMap<>();
+
+        students.put("Anshu", 95);
+        students.put("Riya", 90);
+        students.put("Rahul", 85);
+
+        System.out.println(students);
+    }
+}
