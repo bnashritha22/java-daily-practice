c7a811754566623703474cb14864562b60f6707c
BN Ashritha <bnashritha22@gmail.com>
2026-03-12 17:58:46 +0530

    Create Day23_HashSetExample.java
    Day 23 — Created basic HashSet example

---
+++ Day23_HashSetExample.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+
+public class Day23_HashSetExample {
+
+    public static void main(String[] args) {
+
+        HashSet<String> fruits = new HashSet<>();
+
+        fruits.add("Apple");
+        fruits.add("Mango");
+        fruits.add("Banana");
+
+        System.out.println(fruits);
+    }
+}
