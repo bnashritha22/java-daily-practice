9963fd20eeee58b5917e42e9cf61a6bc8afffdac
BN Ashritha <bnashritha22@gmail.com>
2026-03-14 23:12:50 +0530

    Create Day25_ExpenseTracker.java
    Day 25 — Stored expenses in ArrayList

---
+++ Day25_ExpenseTracker.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Day25_ExpenseTracker {
+
+    public static void main(String[] args) {
+
+        ArrayList<Expense> expenses = new ArrayList<>();
+
+        expenses.add(new Expense("Food", 250));
+        expenses.add(new Expense("Transport", 120));
+        expenses.add(new Expense("Shopping", 600));
+
+        for(Expense e : expenses) {
+            e.display();
+        }
+    }
+}
