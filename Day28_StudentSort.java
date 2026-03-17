5f321bc41fd7e65a3bf243336ed8aee3b150798c
BN Ashritha <bnashritha22@gmail.com>
2026-03-17 19:09:21 +0530

    Create Day28_StudentSort.java
    Day 28 — Sorted students by marks using Comparator

---
+++ Day28_StudentSort.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Day28_StudentSort {
+
+    public static void main(String[] args) {
+
+        ArrayList<Student> students = new ArrayList<>();
+
+        students.add(new Student("Anshu", 95));
+        students.add(new Student("Riya", 90));
+        students.add(new Student("Rahul", 85));
+
+        Collections.sort(students, new Comparator<Student>() {
+            public int compare(Student s1, Student s2) {
+                return s2.marks - s1.marks; // descending order
+            }
+        });
+
+        for(Student s : students) {
+            System.out.println(s.name + " : " + s.marks);
+        }
+    }
+}
