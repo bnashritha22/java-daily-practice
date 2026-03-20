d9202a773503fb2ee3e7901474365533d9dc6346
BN Ashritha <bnashritha22@gmail.com>
2026-03-20 11:26:03 +0530

    Update Day01_HelloWorld.java
    Updated

--- Day01_HelloWorld.java
+++ Day01_HelloWorld.java
@@ -1,5 +1,5 @@
 public class Day01_HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Day 1 — Java streak started!");
+        System.out.println("Day 1 — Java streak started!"); 
     }
 }
