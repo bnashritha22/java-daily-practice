9e631238e64b67139c0f664212709e2aea835157
BN Ashritha <bnashritha22@gmail.com>
2026-03-22 22:36:26 +0530

    Update Day06_EvenNumbers.java
    updated

--- Day06_EvenNumbers.java
+++ Day06_EvenNumbers.java
@@ -1,6 +1,6 @@
 public class Day06_EvenNumbers {
     public static void main(String[] args) {
-        int num = 2;
+        int num = 2; 
 
         while (num <= 10) {
             System.out.println(num);
