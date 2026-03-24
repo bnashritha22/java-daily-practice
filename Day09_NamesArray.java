4e9f7e815f9995650f8c1f7c667ccf2521c18791
BN Ashritha <bnashritha22@gmail.com>
2026-03-24 22:28:35 +0530

    Update Day09_NamesArray.java
    updated

--- Day09_NamesArray.java
+++ Day09_NamesArray.java
@@ -1,7 +1,7 @@
 public class Day09_NamesArray {
     public static void main(String[] args) {
 
-        String[] names = {"Ashritha", "Ravi", "Priya"};
+        String[] names = {"Ashritha", "Ravi" , "Priya"};
 
         for (int i = 0; i < names.length; i++) {
             System.out.println(names[i]);
