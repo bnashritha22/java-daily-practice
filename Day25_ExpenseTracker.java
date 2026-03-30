b30f5b979d55cd237984bd02910f4d26cbd34e88
BN Ashritha <bnashritha22@gmail.com>
2026-03-31 05:12:04 +0530

    Update Day25_ExpenseTracker.java
    updated

--- Day25_ExpenseTracker.java
+++ Day25_ExpenseTracker.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Day25_ExpenseTracker {
 
-    public static void main(String[] args) {
+    public static void main( String[] args) {
 
         ArrayList<Expense> expenses = new ArrayList<>();
 
