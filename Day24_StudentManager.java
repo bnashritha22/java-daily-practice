c53b3db8fd0e56ac501a15459f2ab908ef12504d
BN Ashritha <bnashritha22@gmail.com>
2026-03-31 05:11:21 +0530

    Update Day24_StudentManager.java
    updated

--- Day24_StudentManager.java
+++ Day24_StudentManager.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Day24_StudentManager {
 
-    public static void main(String[] args) {
+    public static void main( String[] args) {
 
         ArrayList<Student> students = newArrayList<>();
 
